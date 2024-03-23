116d0b55f1959276c7ca47d874986b08f75bf01d
Michael Reyesgit config --global user.email michael4reyes7@gmail.comgit statusclearclear <michael4reyes7@gmail.com>
2024-03-22 19:11:11 -0700

    created function to fetch data

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+const DATA = './data.json';
 const clearButton = document.querySelector('.clearButton');
+const jobTags = document.querySelectorAll('.job-tags button');
+let arrayJobTags = Array.from(jobTags);
+
 
 const clearJobTags = (button) => {
     const allTags = document.querySelector('.tags');
@@ -6,9 +10,20 @@ const clearJobTags = (button) => {
     button.addEventListener('click', () => {
         let tag = allTags.children;
         for (let i = 0; i < tag.length - 1; i++) {
-            tag[i].innerHTML = '';
+            tag[i].innerText = '';
         }
     })
 }
 
+const fetchData = async data => {
+    const res = await fetch(data);
+    const jobListings = await res.json();
+    jobListings.map((job, i) => {
+        let jobLanguages = job.languages;
+        let jobTools = job.tools;
+        console.log('Languages: ', jobLanguages, '--- Tools: ', jobTools);
+    })
+}
+
 clearJobTags(clearButton);
+fetchData(DATA);
